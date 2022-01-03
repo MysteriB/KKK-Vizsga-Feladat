4f7935217d39b349f44b55be6e27dfd8e49f5da6
MysteriB <planktomi169@gmail.com>
2022-01-03 16:55:49 +0100

    Try and Error

--- Program/Programozott Hálózat.py
+++ Program/Programozott Hálózat.py
@@ -1,8 +1,5 @@
 import paramiko
 import time
-from host import network_devices
-from config import host_conf
-
 
 UN = input("Username : ")
 PW = input("Password : ")
@@ -10,19 +7,22 @@ PW = input("Password : ")
 
 host = "192.168.1.12"
 port = 22
-command = "ls"
 i = 0
 #copy running-config ftp
 #localip
-#ssh = paramiko.SSHClient()
-#ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#ssh.connect(host, port, UN, PW)
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, UN, PW)
 while i < 1:
     command = input("command : ")
     if command == "exit":
         i = 1
     else:
-        #stdin, stdout, stderr = ssh.exec_command(command)
-        #lines = stdout.readlines()
-        #print(lines)
-        print(command)
+        command = "paru -S openspades"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        lines = stdout.readlines()
+        print(lines)
+        command = "Y"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        lines = stdout.readlines()
+        print(lines)
