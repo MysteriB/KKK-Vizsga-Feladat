713736c3c204a83edef6be197b4cea4ae6b2955d
MysteriB <andor169@hotmail.com>
2022-01-04 08:14:21 +0100

    Save Test

--- save.bat
+++ save.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p com= "Commit Text : " 
 git.exe checkout main
-git.exe pull
+git.exe pull https://github.com/MysteriB/KKK-Vizsga-Feladat.git
 git.exe add -A
 git.exe commit -m "%com%"
 git.exe push --set-upstream origin main
